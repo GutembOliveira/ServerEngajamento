2ebd67d454cb0ed00a55785e28ba184d428a7cc7
Guilherme Santos <santosgui678@gmail.com>
2024-07-01 20:40:12 -0300

    React native elements + env

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,13 +1,49 @@
 import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5'
+//import Icon from 'react-native-vector-icons/FontAwesome5'
 //import Card from '../components/Card';
 import globalStyles from '../utils/globalStyles';
 import theme from '../theme';
 
+import { Button } from '@rneui/themed';
+
 export default function HomeScreen({ navigation }) {
   return (
     <SafeAreaView style={globalStyles.container}>
-      <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('Teacher')}>
+      <Button
+      onPress={() => navigation.navigate('Teacher')}
+      size="lg"
+      radius={"lg"}
+      color={theme.colors.darkGray}
+      icon={{ name: 'chalkboard-teacher', type: 'font-awesome-5', size: 15}}
+      containerStyle={{
+        width: Dimensions.get('window').width * 0.75,
+        elevation: 3,
+        marginVertical: 20,
+      }}
+      iconContainerStyle={{ marginRight: 30}}
+      buttonStyle={{ padding: 20}}
+      titleStyle={{ color: 'black'}}>
+        Sou professor
+      </Button>
+
+      <Button
+      onPress={() => navigation.navigate('Student')}
+      size="lg"
+      radius={"lg"}
+      color={theme.colors.darkGray}
+      icon={{ name: 'book', type: 'font-awesome-5', size: 15}}
+      containerStyle={{
+        width: Dimensions.get('window').width * 0.75,
+        elevation: 3,
+        marginVertical: 20,
+      }}
+      iconContainerStyle={{ marginRight: 30}}
+      buttonStyle={{ padding: 20}}
+      titleStyle={{ color: 'black'}}>
+        Sou aluno
+      </Button>
+
+      {/* <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('Teacher')}>
         <View style={styles.cardContent}>
             <Icon name='chalkboard-teacher'/>
             <Text>Sou professor</Text>
@@ -19,7 +55,7 @@ export default function HomeScreen({ navigation }) {
           <Icon name='book'/>
           <Text>Sou aluno</Text>
         </View>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </SafeAreaView>
   )
 }
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   //baseURL: 'https://quizapp-backend-532h.onrender.com'
   //baseURL: 'http://192.168.0.105:5000'
   //baseURL: 'https://quiz-app-new-backend.onrender.com'
-  baseURL: 'http://34.234.206.222:5000'
+  baseURL: process.env.EXPO_PUBLIC_URL_API
 });
 
 export default api;
