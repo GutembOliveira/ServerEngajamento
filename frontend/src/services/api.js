5cd9721e5914c68907c8396917567e793feca1d4
Guilherme Santos <santosgui678@gmail.com>
2024-07-03 10:49:02 -0300

    Update expo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'https://quizapp-backend-532h.onrender.com'
+  baseURL: 'https://quizapp-backend-532h.onrender.com'
   //baseURL: 'http://192.168.0.105:5000'
   //baseURL: 'https://quiz-app-new-backend.onrender.com'
-  baseURL: process.env.EXPO_PUBLIC_URL_API
+  //baseURL: process.env.EXPO_PUBLIC_URL_API
 });
 
 export default api;
