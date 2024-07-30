5850a30d7f707fed4f38750c383f8f4c9a87441c
Guilherme Santos <santosgui678@gmail.com>
2024-07-30 12:12:06 -0300

    App bar

--- apps/teacher-web/src/App.jsx
+++ apps/teacher-web/src/App.jsx
@@ -1,8 +1,20 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 export default function App() {
   return (
     <>
-      <header>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Engajamento
+          </Typography>
+          <Button color="inherit">Questionários</Button>
+          <Button color="inherit">Turmas</Button>
+        </Toolbar>
+      </AppBar>
     </>
 
   )
