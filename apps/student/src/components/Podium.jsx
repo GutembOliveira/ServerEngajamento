2e91a717f2d09934ba534409f1443c67b58db160
Guilherme Santos <santosgui678@gmail.com>
2024-08-04 10:45:18 -0300

    Incluindo medalhas nos podios

--- apps/student/src/components/Podium.jsx
+++ apps/student/src/components/Podium.jsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Icon, Text } from 'react-native-paper';
 
 const Podium = ({ students }) => {
   // Ordena alunos por pontuação
-  const sortedStudents = students.sort((a, b) => b.pontuacao - a.pontuacao).slice(0, 3);
+  //const sortedStudents = students.sort((a, b) => b.pontuacao - a.pontuacao).slice(0, 3);
+  const podiumStudents = students.slice(0, 3);
+
+  const setMedalColor = (index) => {
+    if (index === 0) {
+      return '#a57c00'
+    } else if (index === 1) {
+      return '#c0c0c0'
+    } else if (index === 2) {
+      return '#cd7f32'
+    }
+  }
 
   const renderPodiumItem = ({ item, index }) => (
     <View style={styles.podiumItem}>
-      <Text variant='titleSmall' style={{ marginTop: 10 }}>{index + 1}º</Text>
-      <Text variant='titleSmall' style={{ marginVertical: 10}}>{item.nome}</Text>
-      <Text variant='titleSmall'>Pontuação: {item.pontuacao}</Text>
+      <View style={{ display: 'flex', flexDirection: 'row'}}>
+        <Text variant='headlineSmall' style={{ marginTop: 10 }}>
+          {index + 1}º Lugar
+        </Text>
+        <Icon source="medal" color={setMedalColor(index)} size={50} />
+      </View>
+      <Text variant='titleLarge' style={{ marginVertical: 10 }}>{item.nome}</Text>
+      <Text variant='titleLarge'>{item.pontuacao} acertos</Text>
     </View>
   );
 
   return (
     <View style={styles.container}>
-      <Text variant='titleMedium'>Pódio</Text>
+      <Text variant='headlineLarge'>Pódio</Text>
       <FlatList
         style={{ flexGrow: 0 }}
-        data={sortedStudents}
+        data={podiumStudents}
         renderItem={renderPodiumItem}
         keyExtractor={(item) => item.matricula.toString()}
       />
@@ -34,7 +50,7 @@ const styles = StyleSheet.create({
   },
   podiumItem: {
     alignItems: 'center',
-    marginVertical: 10,
+    marginVertical: 10
   }
 });
 
