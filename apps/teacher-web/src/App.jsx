79c5c392bc771615dc4480f9f51d45d74bdbd1fb
Guilherme Santos <santosgui678@gmail.com>
2024-08-26 20:22:20 -0300

    Mudando navegação para sidebar

--- apps/teacher-web/src/App.jsx
+++ apps/teacher-web/src/App.jsx
@@ -1,24 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
-import Home from './pages/Home';
-import Quizzes from './pages/Quizzes';
-import Classes from './pages/Classes';
-import ResponsiveAppBar from './components/ResponsiveAppBar';
+import PersistentDrawerLeft from './components/PersistentDrawerLeft';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={darkTheme}>
       <CssBaseline/>
 
-      <ResponsiveAppBar/>
+      <PersistentDrawerLeft/>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="Quizzes" element={<Quizzes />} />
-        <Route path="Classes" element={<Classes />} />
-
-
-      </Routes>
-    </div>
+      
+    </ThemeProvider>
+  
   )
 }
