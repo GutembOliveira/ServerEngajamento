df4ed22f6af6b9eed884e1f6137985ff9aff53a5
Guilherme Santos <santosgui678@gmail.com>
2024-09-01 10:13:04 -0300

    Incluindo cards de listagem

--- apps/teacher-web/src/pages/Classes.jsx
+++ apps/teacher-web/src/pages/Classes.jsx
@@ -1,7 +1,86 @@
+import { Card, CardContent, CardMedia, CardActionArea, Typography, Grid } from '@mui/material';
+
+import classImage from '../assets/class.jpeg';
+
 export default function Classes(){
     return(
         <div>
-            <h2>Classes</h2>
+            <h2>Minhas turmas</h2>
+
+            <Grid container spacing={2}>
+                <Grid item xs={4}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={classImage}
+                                alt="Turma"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Turma
+                                </Typography>
+
+                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    Turma Teste
+                                </Typography>
+
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={classImage}
+                                alt="Turma"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Turma
+                                </Typography>
+
+                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    Turma Teste
+                                </Typography>
+
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={classImage}
+                                alt="Turma"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Turma
+                                </Typography>
+
+                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    Turma Teste
+                                </Typography>
+
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+
+                </Grid>
+
+            </Grid>
         </div>
     )
 }
--- apps/teacher-web/src/pages/Quizzes.jsx
+++ apps/teacher-web/src/pages/Quizzes.jsx
@@ -1,7 +1,105 @@
-export default function Quizzes(){
-    return(
+import { Card, CardContent, CardMedia, CardActionArea, CardActions, Typography, Grid, Button } from '@mui/material';
+
+import quizImage from '../assets/quiz.jpeg';
+
+export default function Quizzes() {
+    return (
         <div>
-            <h2>Quizzes</h2>
+            <h2>Meus Quizzes</h2>
+
+            <Grid container spacing={2}>
+                <Grid item xs={4}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={quizImage}
+                                alt="Quiz"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Quiz
+                                </Typography>
+
+                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    Quiz Teste
+                                </Typography>
+
+                            </CardContent>
+                        </CardActionArea>
+
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Iniciar Quiz
+                            </Button>
+                        </CardActions>
+                    </Card>
+
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={quizImage}
+                                alt="Quiz"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Quiz
+                                </Typography>
+
+                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    Quiz Teste
+                                </Typography>
+
+                            </CardContent>
+                        </CardActionArea>
+
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Iniciar Quiz
+                            </Button>
+                        </CardActions>
+                    </Card>
+
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={quizImage}
+                                alt="Quiz"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Quiz
+                                </Typography>
+
+                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    Quiz Teste
+                                </Typography>
+
+                            </CardContent>
+                        </CardActionArea>
+
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Iniciar Quiz
+                            </Button>
+                        </CardActions>
+                    </Card>
+
+                </Grid>
+
+            </Grid>
+
         </div>
     )
 }
